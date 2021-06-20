db1f55cbb964a62c5d79edb9f1b3dc50e593f179
ubaidhsayed <79779255+ubaidhsayed@users.noreply.github.com>
2021-06-20 09:42:56 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world----- pavan is your Trainer !! Hi or Bye is the string needed here.... added";
+    return "Hello world----- Pipeline started pavan is your Trainer !! Hi or Bye is the string needed here.... added";
   }
 }
